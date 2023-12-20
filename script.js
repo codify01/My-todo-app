dffa60812d6bcd42143c7e4ae80d4f8d937ff952
Uthman Oluwaseun <amoleuthman@gmail.com>
2023-12-20 09:28:44 +0100

    modifying

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var todoArray = [];
 function addTodo() {
     let myTodo = document.getElementById('todoinp').value
     if (myTodo != ""){
-        todoArray.push(myTodo)
+        let todoItem = {name: myTodo, done:false}
+        todoArray.push(todoItem)
         todoinp.value = ""
         todoinp.focus()
         showTodo()
@@ -15,11 +16,20 @@ function showTodo() {
     let todoContent = ""
     let i = 0;
     for (i = 0; i < todoArray.length; i++) {
-        todoContent += `<div class="shadow m-2 p-2 mb-1">${todoArray[i]}</div>`
+        if (todoArray[i].done){
+            todoContent += `<div class="shadow m-2 p-2 mb-1 text-success">${todoArray[i].name}</div>`
+        } else {
+            todoContent += `<div class="shadow m-2 p-2 mb-1 text-danger">${todoArray[i].name} <button class="btn-danger btn-sm float-right" onclick="markAsDone">Mark as done</button></div>`
+        }
            
         document.getElementById('todoArea').innerHTML = todoContent
     }   
 }
 function clearTodo() {
     todoArray.length = 0;
+    showTodo()
+}
+
+function markAsDone() {
+    
 }
